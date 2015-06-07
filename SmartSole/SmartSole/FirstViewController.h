ca0cadea26eeec9bcd50d3166ab5e9169e4ae43e
David Jeong <djeong747@gmail.com>
2015-06-07 18:27:08 -0400

    Intial Commit.

---
+++ SmartSole/SmartSole/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SmartSole
+//
+//  Created by David Jeong on 2015. 6. 7..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ SmartSole/SmartSole/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SmartSole
+//
+//  Created by David Jeong on 2015. 6. 7..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
