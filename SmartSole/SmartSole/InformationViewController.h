87da7cff3e05c4be1187b5ce18e88cadff65876e
David Jeong <djeong747@gmail.com>
2015-06-22 12:13:08 -0400

    Removed the splitviewcontroller, instead added tableview with search functionality.

--- SmartSole/SmartSole/InformationViewController.h
+++ SmartSole/SmartSole/InformationViewController.h
@@ -2,12 +2,18 @@
 //  InformationViewController.h
 //  SmartSole
 //
-//  Created by David Jeong on 2015. 6. 21..
+//  Created by David Jeong on 2015. 6. 22..
 //
 //
 
 #import <UIKit/UIKit.h>
 
-@interface InformationViewController : UISplitViewController
+@interface InformationViewController : UIViewController <UISearchBarDelegate, UISearchResultsUpdating, UITableViewDataSource>
+
+@property (strong, nonatomic) UISearchController *searchController;
+@property (strong, nonatomic) NSArray *tableData;
+@property (strong, nonatomic) NSArray *searchResults;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- SmartSole/SmartSole/InformationViewController.m
+++ SmartSole/SmartSole/InformationViewController.m
@@ -2,7 +2,7 @@
 //  InformationViewController.m
 //  SmartSole
 //
-//  Created by David Jeong on 2015. 6. 21..
+//  Created by David Jeong on 2015. 6. 22..
 //
 //
 
@@ -16,22 +16,74 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
+    self.searchController.searchBar.delegate = self;
+    // Fit the search bar to view.
+    [self.searchController.searchBar sizeToFit];
+    
+    // Create a dummy array.
+    self.tableData = [NSArray arrayWithObjects:@"Lorem", @"Ipsum", @"Dolor", @"Sit Amet", nil];
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    self.definesPresentationContext = YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+    NSString *searchString = searchController.searchBar.text;
+    if (searchString != nil && [searchString length] != 0) {
+        [self filterContentForSearchText:searchString scope:nil];
+    } else {
+        self.searchResults = self.tableData;
+    }
+    [self.tableView reloadData];
 }
-*/
+
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope {
+    // Filter the results using a predicate.
+    NSPredicate *resultPredicate = [NSPredicate
+                                    predicateWithFormat:@"SELF contains[cd] %@",
+                                    searchText];
+    
+    self.searchResults = [self.tableData filteredArrayUsingPredicate:resultPredicate];
+}
+
+- (void) searchBar:(UISearchBar *)searchBar selectedScopeButtonIndexDidChange:(NSInteger)selectedScope {
+    [self updateSearchResultsForSearchController:self.searchController];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (self.searchController.active) {
+        return [self.searchResults count];
+    } else {
+        return [self.tableData count];
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        // If cell is nil, create a new cell.
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    if (self.searchController.active) {
+        cell.textLabel.text = [self.searchResults objectAtIndex:indexPath.row];
+        return cell;
+    } else {
+        cell.textLabel.text = [self.tableData objectAtIndex:indexPath.row];
+        return cell;
+    }
+}
 
 @end
