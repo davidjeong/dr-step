09b6778f7c35a07eaf5596a5a51a63de291e0f9a
David Jeong <djeong747@gmail.com>
2015-07-08 19:24:25 -0400

    Changed SmartSole to BioWear.

---
+++ SmartSole/SmartSole/BWSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  SmartSole
+//
+//  Created by David Jeong on 2015. 7. 8..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
